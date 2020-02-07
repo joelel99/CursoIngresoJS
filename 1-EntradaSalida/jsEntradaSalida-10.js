7ef993a3be939957a56752e215f126352396a925
joelel99 <60608459+joelel99@users.noreply.github.com>
2020-02-07 15:00:51 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var sueldo;
+    var aumento;
+    var restfinal;
+
+    sueldo = document.getElementById("importe").value;
+
+    sueldo = parseInt(sueldo);
+    
+    aumento = sueldo * 10 / 100;
+
+    restfinal = sueldo - aumento;
+
+    document.getElementById("resultado").value = restfinal;
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var aumento;
+    var restfinal;
 
-	
+    sueldo = document.getElementById("sueldo").value;
+
+    sueldo = parseInt(sueldo);
+    
+    aumento = sueldo * 10 / 100;
+
+    restfinal = sueldo + aumento;
+
+    document.getElementById("resultado").value = restfinal;
+
+
 }
